fd925b2ba53ef24081e471833cee404e67b378c8
Nau <104866867+NaeN4e@users.noreply.github.com>
2023-02-07 10:33:07 -0300

    Add files via upload

---
+++ PROJETO-JOGO DAS CORES/PRO-V2-C32-Modelo-main/sketch.js
@@ -0,0 +1,15 @@
+var quadrado1,quadrado2,quadrado3,quadrado4,quadrado5,quadrado6,quadrado7,quadrado8,quadrado9;
+
+
+function setup() {
+ createCanvas(600,600)
+  quadrado1 = createSprite( 20,20,60,60 )
+}
+  
+
+
+
+function draw() {
+  background("black")
+  drawSprites();
+}
